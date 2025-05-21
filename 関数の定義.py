4db6bbd8e39fa06aaaf0c2d01c730bf7c00934fd
Fri@-@ <furidausu@outlook.jp>
2025-05-21 11:59:27 +0900

    Update 関数の定義.py
    もしも使い方がわからない場合はDiscussionsでコメントしていただくか、コメントよろしくお願いします。　
    またバグのお知らせもください。　

--- 関数の定義.py
+++ 関数の定義.py
@@ -1,3 +1,13 @@
+# 関数の定義.py
+
 def greet(name):
-    return f"こんにちは、{name}さん！"
-#これは　名前をユーザーさんに入力してもらってその名前を使って{name}がtarouの場合　こんにちは、tarouさん！と出力されます。
+    if name == "tarou":
+        print(f"こんにちは、{name}さん！")
+    else:
+        print(f"{name}さん、こんにちは！")
+
+# ユーザーから名前を入力してもらう
+user_name = input("あなたの名前を入力してください: ")
+
+# 関数を実行する
+greet(user_name)
